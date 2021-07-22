07af070b27a8bb0e7b36ac2341addb9d95523737
jeny797 <jeny797@gmail.com>
2021-07-22 13:30:09 +0300

    initial commit

---
+++ src/models.py
@@ -0,0 +1,42 @@
+from src import db
+
+student_course_association = db.Table(
+    'student_course',
+    db.Column('student_id', db.Integer, db.ForeignKey('students.id')),
+    db.Column('courses_id', db.Integer, db.ForeignKey('courses.id'))
+    )
+
+
+class Student(db.Model):
+
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+
+    group = db.relationship('Group')
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
+
+    def __repr__(self):
+        return f"{self.id}-{self.first_name}-{self.last_name}"
+
+
+class Group(db.Model):
+
+    __tablename__ = 'groups'
+    id = db.Column(db.Integer, primary_key=True)
+    group_name = db.Column(db.String, nullable=False)
+    students = db.relationship('Student')
+
+    def __repr__(self):
+        return f"{self.id}-{self.group_name}"
+
+
+class Course(db.Model):
+
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"{self.id}-{self.course_name}"
